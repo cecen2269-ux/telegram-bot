d090b2a37063f0b5e08e15f050f3dda026b5a092
cecen2269-ux <cecen2269@gmail.com>
2026-01-02 18:13:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = 8403759105:AAEs7u9LZqQX7bWhITpFpZjG57-zz1ekG7s
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Merhaba 👋 Bot çalışıyor!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Bot çalışıyor...")
+app.run_polling()
 
